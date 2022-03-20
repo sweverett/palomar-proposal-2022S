5caad2112ee2f46e715519ee27eb2c9971478ebf
Spencer Everett <spencerweverett@gmail.com>
2022-03-20 13:43:08 -0700

    Restructed `add_cols.py` to be done through a run config

--- sample/add_cols.py
+++ sample/add_cols.py
@@ -4,19 +4,8 @@ import pudb
 
 parser = ArgumentParser()
 
-parser.add_argument('cluster_file', type=str,
-                    help='Filepath for cluster file to add cols to')
-parser.add_argument('source_file', type=str,
-                    help='Filepath for source file to add cols to')
-parser.add_argument('-start_date', type=str, default='2022-08-01',
-                    help='Starting date for search period')
-parser.add_argument('-end_date', type=str, default='2023-01-31',
-                    help='Ending date for search period')
-parser.add_argument('-utc_offset', type=int, default=-7,
-                    help='UTC offset for observatory (currently doesnt) ' +\
-                    'account for time zones')
-parser.add_argument('-min_airmass', type=float, default=1.5,
-                    help='Minimum required airmass for a target exposure')
+parser.add_argument('config_file', type=str,
+                    help='Filepath for run config')
 parser.add_argument('-outfile', type=str, default=None,
                     help='Filepath for output file w/ added cols')
 parser.add_argument('--overwrite', action='store_true', default=False,
@@ -24,19 +13,29 @@ parser.add_argument('--overwrite', action='store_true', default=False,
 
 def main(args):
 
-    cluster_file = args.cluster_file
+    config = args.config_file
     outfile = args.outfile
-    start_date = args.start_date
-    end_date = args.end_date
-    utc_offset = args.utc_offset
-    min_airmass = args.min_airmass
     overwrite = args.overwrite
 
-    print(f'Adding cols to cluster file {cluster_file}...')
-    add_good_nights_col(
-        cluster_file, start_date, end_date, utc_offset,
-        min_airmass=min_airmass, overwrite=overwrite
-        )
+    cluster_file = config['cluster_file']
+    source_file = config['source_file']
+
+    #-----------------------------------------------------------------
+    if 'airmass' in config['run']:
+        print(f'Adding cols to cluster file {cluster_file}...')
+
+        start_date = config['airmass']['start_date']
+        end_date = config['airmass']['end_date']
+        utc_offset = config['airmass']['utc_offset']
+        min_airmass = config['airmass']['min_airmass']
+        add_good_nights_col(
+            cluster_file, start_date, end_date, utc_offset,
+            min_airmass=min_airmass, overwrite=overwrite
+            )
+
+    #-----------------------------------------------------------------
+    if 'match' in config['run']:
+        print(f'Matching clusters to sources in {source_file}...')
 
     return 0
 
