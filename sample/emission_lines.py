8653196d3b14db7df130bf5d919b2929e167e493
Spencer Everett <spencerweverett@gmail.com>
2022-03-20 16:20:16 -0700

    Renamed file to `emission_lines.py` which will store all related funcs

--- sample/emission_lines.py
+++ sample/emission_lines.py
@@ -1,10 +1,15 @@
 import numpy as np
 from astropy.table import Table
 
-def compute_visible_lines(cat, lines, blue_lim, red_lim, plot=False):
+def compute_line_sb(source_file, fiber_area):
+    pass
+
+def compute_visible_lines(source_file, lines, blue_lim, red_lim, outfile=None,
+                          overwrite=False, plot=False):
     '''
     This function computes how many emission lines are visible
-    in the given catalog and filter boundaries
+    in the given catalog and filter boundaries and saves to an
+    output catalog
 
     lines: dict
         A dictionary in the format of `line_name`: wavelength
@@ -12,6 +17,15 @@ def compute_visible_lines(cat, lines, blue_lim, red_lim, plot=False):
     blue/red_lim: float
         The filter limits in nm
     '''
+
+    if outfile is None:
+        if overwrite is False:
+            raise Exception('Must either pass an outfile or set '\
+                            'overwrite=True')
+        else:
+            outfile = source_file
+
+    cat = Table.read(source_file)
 
     N = len(cat)
 
@@ -30,6 +44,8 @@ def compute_visible_lines(cat, lines, blue_lim, red_lim, plot=False):
 
         cat['visible_lines'][visible] += 1
 
+    cat.write(outfile, overwrite=overwrite)
+
     if plot is True:
         plt.hist(cat['visible_lines'])
         plt.xlabel('Number of visible emission lines')
@@ -39,4 +55,4 @@ def compute_visible_lines(cat, lines, blue_lim, red_lim, plot=False):
         plt.gcf().set_size_inches(9,6)
         plt.show()
 
-    return cat
+    return
