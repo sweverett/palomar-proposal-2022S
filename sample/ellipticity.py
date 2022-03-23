99086e610a49f3ded01d6d5d9269eafa2e75739d
Spencer Everett <spencerweverett@gmail.com>
2022-03-23 12:41:06 -0700

    Added new ellipticity calc function

---
+++ sample/ellipticity.py
@@ -0,0 +1,38 @@
+import numpy as np
+import utils
+
+def compute_ellipticity(sources, indx=2, plot=False):
+    '''
+    indx is band indx (ugriz)
+    '''
+
+    AB_EXP = sources['AB_EXP']
+
+    e = (1 - (AB_EXP)**2 ) / (1 + (AB_EXP)**2)
+
+    sources['e'] = e
+
+    if plot is True:
+        print('Making ellipticity plots...')
+
+        plt.hist(e, ec='k', bins=50)
+        plt.xlabel('Ellipticity (1-(A/B)^2) / (1+(A/B)^2)')
+        plt.ylabel('Counts')
+        plt.yscale('log')
+        plt.gcf().set_size_inches(8,5)
+
+        plotfile = os.path.join(utils.get_plot_dir(), 'ellipticity_all.png')
+        plt.savefig(plotfile, bbox_inches='tight', dpi=300)
+        plt.close()
+
+        plt.hist(e, ec='k', bins=np.lispace(0,1,50))
+        plt.xlabel('Ellipticity (1-(A/B)^2) / (1+(A/B)^2)')
+        plt.ylabel('Counts')
+        plt.yscale('log')
+        plt.gcf().set_size_inches(8,5)
+
+        plotfile = os.path.join(utils.get_plot_dir(), 'ellipticity_bounds.png')
+        plt.savefig(plotfile, bbox_inches='tight', dpi=300)
+        plt.close()
+
+    return sources
